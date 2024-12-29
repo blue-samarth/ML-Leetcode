e72c010bbb2bfea981874c9c06279c7fb9f70194
blue-samarth <samarth38work@gmail.com>
2024-12-29 21:35:43 +0530

    matrix dot vector

---
+++ matrix_dot_vector.py
@@ -0,0 +1,13 @@
+def matrix_dot_vector(a:list[list[int|float]],b:list[int|float])-> list[int|float] | int:
+	row_a : int = len(a)
+	row_b : int = len(b)
+	col_a : int = len(a[0])
+	res : list = []
+	# since matrix can be multiplied when col_a == row_b
+	if col_a != row_b: return -1
+	for row in a:
+		cur : int | float = 0
+		for j in range(col_a):
+			cur += row[j] * b[j]
+		res.append(cur)
+	return res
